b0aae0113787a766a7505dd3992907e998319c2a
Manav Yadav <manav.y1998@gmail.com>
2023-01-11 00:58:31 +0530

    Reverse ll (recursion)

--- node.py
+++ node.py
@@ -25,18 +25,18 @@ class linkedList(node):
             itr = itr.nextNode
         return itr
 
-    def incert(self, value, position):
+    def insert(self, value, position):
         oldNode = self.iterate(position-1)
         newNode = node(value)
         newNode.nextNode = oldNode.nextNode
         oldNode.nextNode = newNode
 
-    def incert_at_end(self, value):
+    def insert_at_end(self, value):
         tail = self.get_tail()
         newNode = node(value)
         tail.nextNode = newNode
     
-    def incert_at_start(self, value):
+    def insert_at_start(self, value):
         newNode = node(value)
         newNode.nextNode = self.head
         self.head = newNode
@@ -57,17 +57,41 @@ class linkedList(node):
         return
 
     def Print_rev(self):
-        self.rev_rec(self.head)
+        if self.head != None:
+            self.rev_rec(self.head)
+        else:
+            print('Empty Linked List.')
+            return
+    
+    def rev_ll_rec_exe(self, node):
+        if node.nextNode is None:
+            self.head = node
+            return
+        self.rev_ll_rec_exe(node.nextNode)
+        tempNode = node.nextNode
+        tempNode.nextNode = node
+        node.nextNode = None
+        return
+
+    def rev_ll_rec(self):
+        if self.head != None:
+            self.rev_ll_rec_exe(self.head)
+        else:
+            print('Empty Linked List.')
+            return
+        
     
 ll = linkedList()
 
 node1 = node(3)
 ll.head = node1
-ll.incert_at_end(5)
+ll.insert_at_end(5)
 
 for i in range(4):
     val= input('Enter node value : ')
-    ll.incert_at_end(int(val))
-    ll.Print()
-
-ll.Print_rev()
+    ll.insert_at_end(int(val))
+    
+ll.Print()
+print()
+ll.rev_ll_rec()
+ll.Print()
